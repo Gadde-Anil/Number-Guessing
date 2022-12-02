1db5e6cc42f78e61d22d4623274629a0a555f1c9
Gadde-Anil <118916141+Gadde-Anil@users.noreply.github.com>
2022-12-02 18:42:09 +0530

    Add files via upload

---
+++ Numberguessing.java
@@ -0,0 +1,43 @@
+import java.lang.*;
+import java.util.*;
+public class Numberguessing
+{
+    // number guessing game
+	public static void guessingNumberGame()
+	{
+		Scanner sc= new Scanner(System.in);
+        int number= 1 + (int)(100 * Math.random());
+		int K= 7;
+        int i;
+        System.out.println("Number is chosen  between 1 to 100. Guess the number within " + K  + " trials.");
+		for (i = 0; i < K; i++)
+        {
+
+			System.out.println("Guess the number:");
+			int guess = sc.nextInt();
+			if (number == guess) 
+            {
+				System.out.println("Congratulations! You guessed the number.");
+				break;
+			}
+			else if (number > guess && i != K - 1)
+             {
+                System.out.println("The number is greater than " + guess);
+			}
+			else if (number < guess && i != K - 1) 
+            {
+				System.out.println("The number is less than " + guess);
+			}
+		}
+        if (i == K) 
+        {
+			System.out.println("You have exhausted K trials.");
+            System.out.println("The number was " + number);
+		}
+	}
+	public static void main(String arg[])
+	{
+		guessingNumberGame();
+	}
+}
+
